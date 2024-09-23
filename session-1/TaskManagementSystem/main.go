0275f542e6ffb90f409c999eddfb398148a47dbc
orkhan-rafiyev <134998338+orkhan-rafiyev@users.noreply.github.com>
2024-09-23 17:38:29 +0400

    Session-1: First Project

---
+++ session-1/TaskManagementSystem/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the Task Management System!")
+	fmt.Println("start date is: 2024-09-18 00:00:00")
+	fmt.Println("Project: Task Management System")
+}
